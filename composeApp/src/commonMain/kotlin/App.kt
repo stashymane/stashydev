f449a87d419d8bb9810b29398968bfe29c057679
stashymane <aestheticrice@gmail.com>
2024-12-23 01:46:16 +0200

    add dots background

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,23 +1,29 @@
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionLayout
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageShader
+import androidx.compose.ui.graphics.ShaderBrush
 import androidx.compose.ui.layout.ContentScale
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import coil3.compose.AsyncImage
+import coil3.compose.LocalPlatformContext
+import coil3.request.ImageRequest
+import coil3.request.crossfade
 import components.AppNavbar
 import dev.stashy.home.Res
+import dev.stashy.home.texture
 import kotlinx.serialization.json.Json
 import locals.LocalNavController
 import locals.LocalSharedTransitionScope
 import model.MainViewModel
 import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.imageResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.viewmodel.koinViewModel
 import theme.AppTheme
@@ -31,8 +37,11 @@ val json = Json {
 fun App(vm: MainViewModel = koinViewModel()) {
     val navController = rememberNavController()
 
-//    val dots = imageResource(Res.drawable.texture)
-//    val brush = ShaderBrush(ImageShader(dots))
+    val dotsBrush = ShaderBrush(ImageShader(imageResource(Res.drawable.texture)))
+    val background = ImageRequest.Builder(LocalPlatformContext.current)
+        .data(Res.getUri("drawable/background.jpg"))
+        .crossfade(true)
+        .build()
 
     AppTheme(Color.Red, true) {
         CompositionLocalProvider(LocalNavController provides navController) {
@@ -41,11 +50,13 @@ fun App(vm: MainViewModel = koinViewModel()) {
             }) { contentPadding ->
                 Box {
                     AsyncImage(
-                        Res.getUri("drawable/background.jpg"),
+                        background,
                         null,
-                        Modifier.fillMaxSize(),
+                        Modifier.matchParentSize(),
                         contentScale = ContentScale.FillBounds
                     )
+                    Box(Modifier.matchParentSize().background(dotsBrush))
+                    
                     SharedTransitionLayout {
                         CompositionLocalProvider(LocalSharedTransitionScope provides this@SharedTransitionLayout) {
                             Navigation(contentPadding)
