d6a789ad28968737a6ff0c9b1b16fac27a1aad00
stashymane <aestheticrice@gmail.com>
2024-12-23 02:06:19 +0200

    consolidate httpclient into common

--- composeApp/src/commonMain/kotlin/HttpClient.kt
+++ composeApp/src/commonMain/kotlin/HttpClient.kt
@@ -1,4 +1,16 @@
 import io.ktor.client.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.logging.*
+import io.ktor.client.plugins.resources.*
+import io.ktor.serialization.kotlinx.json.*
 
-expect val httpClient: HttpClient
+val httpClient: HttpClient = HttpClient {
+    install(Logging) {
+        level = LogLevel.INFO
+    }
+    install(Resources)
+    install(ContentNegotiation) {
+        json(json)
+    }
+}
 
