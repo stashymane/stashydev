4fdbff9120a5901158e14d9c3b19a1eb588ee440
stashymane <aestheticrice@gmail.com>
2025-01-07 04:31:10 +0200

    remove background image

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,43 +1,32 @@
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionLayout
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import coil3.ImageLoader
-import coil3.compose.AsyncImage
-import coil3.compose.LocalPlatformContext
 import coil3.compose.setSingletonImageLoaderFactory
 import coil3.decode.SkiaImageDecoder
 import coil3.network.ktor3.KtorNetworkFetcherFactory
-import coil3.request.ImageRequest
 import coil3.request.crossfade
 import coil3.serviceLoaderEnabled
 import coil3.svg.SvgDecoder
 import coil3.util.DebugLogger
 import components.nav.AppNavbar
-import dev.stashy.home.Res
 import kotlinx.serialization.json.Json
 import locals.LocalSharedTransitionScope
-import model.MainViewModel
-import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import org.koin.compose.viewmodel.koinViewModel
 import theme.AppTheme
 
 val json = Json {
     ignoreUnknownKeys = true
 }
 
-@OptIn(ExperimentalResourceApi::class, ExperimentalSharedTransitionApi::class)
+@OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
-fun App(vm: MainViewModel = koinViewModel()) {
+fun App() {
     setSingletonImageLoaderFactory { context ->
         ImageLoader.Builder(context)
             .crossfade(true)
@@ -51,30 +40,16 @@ fun App(vm: MainViewModel = koinViewModel()) {
             .build()
     }
 
-    val background = ImageRequest.Builder(LocalPlatformContext.current)
-        .data(Res.getUri("drawable/background.jpg"))
-        .crossfade(true)
-        .build()
-
     AppTheme(Color.Blue, true) {
         Scaffold(
             containerColor = MaterialTheme.colorScheme.surfaceContainerLowest,
         ) { contentPadding ->
-            Box {
-                AsyncImage(
-                    background,
-                    null,
-                    Modifier.fillMaxSize(),
-                    contentScale = ContentScale.FillBounds
-                )
+            Row {
+                AppNavbar()
 
-                Row {
-                    AppNavbar()
-
-                    SharedTransitionLayout {
-                        CompositionLocalProvider(LocalSharedTransitionScope provides this@SharedTransitionLayout) {
-                            Navigation(contentPadding)
-                        }
+                SharedTransitionLayout {
+                    CompositionLocalProvider(LocalSharedTransitionScope provides this@SharedTransitionLayout) {
+                        Navigation(contentPadding)
                     }
                 }
             }
