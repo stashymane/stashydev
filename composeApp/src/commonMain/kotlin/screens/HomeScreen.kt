03c03297df16d85f9eb33152993c5bd5d3ca4bce
stashymane <me@stashy.dev>
2025-09-18 19:14:46 +0300

    add initial background shader variant

--- composeApp/src/commonMain/kotlin/screens/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/screens/HomeScreen.kt
@@ -1,13 +1,19 @@
 package screens
 
+import androidx.compose.animation.core.withInfiniteAnimationFrameMillis
 import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.graphics.ShaderBrush
 import androidx.compose.ui.unit.dp
 import androidx.window.core.layout.WindowSizeClass
 import components.ResponsiveRow
@@ -19,13 +25,39 @@ import icons.outlinelarge.Cases
 import icons.outlinelarge.FitScreen
 import icons.outlinelarge.UserSearch
 import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.skia.RuntimeShaderBuilder
+import theme.Shaders
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun HomeScreen(contentPadding: PaddingValues) {
     val scrollState = rememberScrollState()
+    val backgroundEffect = remember { Shaders.Pattern }
 
     Box(Modifier.fillMaxSize()) {
+        val time by produceState(0f) {
+            while (true) {
+                withInfiniteAnimationFrameMillis { value = it / 1000f }
+            }
+        }
+
+        Box(Modifier.matchParentSize().drawWithCache {
+            val shader = RuntimeShaderBuilder(backgroundEffect).apply {
+                uniform("iTime", time)
+                uniform("iResolution", size.width, size.height)
+                uniform("Warp2X", 0.1f)
+                uniform("Warp2Y", 0f)
+                uniform("WarpSpeedX", 0f)
+                uniform("WarpSpeedY", 0.1f)
+                uniform("RingStrength", 2f)
+            }.makeShader()
+            val brush = ShaderBrush(shader)
+
+            onDrawBehind {
+                drawRect(brush)
+            }
+        })
+
         Box(Modifier.fillMaxSize().verticalScroll(scrollState), contentAlignment = Alignment.Center) {
             Column(
                 Modifier.widthIn(max = WindowSizeClass.WIDTH_DP_EXPANDED_LOWER_BOUND.dp).padding(contentPadding)
