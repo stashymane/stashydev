f4f00f2b20f7c05787639129f512e5aa414014d5
stashymane <me@stashy.dev>
2026-07-21 15:29:42 +0300

    preload less fonts

--- composeApp/src/webMain/kotlin/ui/ContentLoader.web.kt
+++ composeApp/src/webMain/kotlin/ui/ContentLoader.web.kt
@@ -6,12 +6,8 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.text.font.Font
-import dev.stashy.home.PlayfairDisplay_Italic_VariableFont_wght
 import dev.stashy.home.PlayfairDisplay_VariableFont_wght
 import dev.stashy.home.Res
-import dev.stashy.home.Tomorrow_Italic
-import dev.stashy.home.Tomorrow_Light
-import dev.stashy.home.Tomorrow_LightItalic
 import dev.stashy.home.Tomorrow_Regular
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.preloadFont
@@ -21,11 +17,7 @@ import org.jetbrains.compose.resources.preloadFont
 actual fun loadContent(): State<LoadingState> {
     val fonts: List<State<Font?>> = listOf(
         preloadFont(Res.font.Tomorrow_Regular),
-        preloadFont(Res.font.Tomorrow_Light),
-        preloadFont(Res.font.Tomorrow_Italic),
-        preloadFont(Res.font.Tomorrow_LightItalic),
         preloadFont(Res.font.PlayfairDisplay_VariableFont_wght),
-        preloadFont(Res.font.PlayfairDisplay_Italic_VariableFont_wght),
     )
 
     val images: List<State<ImageBitmap?>> = listOf(
