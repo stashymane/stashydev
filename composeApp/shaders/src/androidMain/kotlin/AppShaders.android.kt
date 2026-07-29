5fc8631234339437140704327a77144cca779450
stashymane <me@stashy.dev>
2026-07-29 12:03:38 +0300

    make shaders work in previews

--- composeApp/shaders/src/androidMain/kotlin/AppShaders.android.kt
+++ composeApp/shaders/src/androidMain/kotlin/AppShaders.android.kt
@@ -1,26 +1,45 @@
+import android.graphics.RuntimeShader
+import android.os.Build
 import androidx.compose.ui.graphics.Shader
 
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
-actual class AppShader : ShaderParams {
+actual class AppShader private constructor(
+    private val shader: android.graphics.Shader
+) : ShaderParams {
     actual companion object {
-        actual fun createGloop(): AppShader {
-            TODO("Not yet implemented")
-        }
+        actual fun createGloop(): AppShader = from(ShaderSources.Gloop)
+        actual fun createTest(): AppShader = from(ShaderSources.Test)
 
-        actual fun createTest(): AppShader {
-            TODO("Not yet implemented")
+        fun from(src: String): AppShader = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            AppShader(RuntimeShader(src))
+        } else {
+            AppShader(
+                android.graphics.LinearGradient(
+                    0f, 0f, 1f, 1f,
+                    android.graphics.Color.TRANSPARENT, android.graphics.Color.TRANSPARENT,
+                    android.graphics.Shader.TileMode.CLAMP
+                )
+            )
         }
-
     }
 
     actual override fun set(param: String, value: Int) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            (shader as RuntimeShader).setIntUniform(param, value)
+        }
     }
 
     actual override fun set(param: String, value: Float) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            (shader as RuntimeShader).setFloatUniform(param, value)
+        }
     }
 
     actual override fun set(param: String, value1: Float, value2: Float) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            (shader as RuntimeShader).setFloatUniform(param, value1, value2)
+        }
     }
 
-    actual fun asComposeShader(): Shader = TODO()
+    actual fun asComposeShader(): Shader = shader
 }
--- composeApp/shaders/src/commonMain/kotlin/ShaderSources.kt
+++ composeApp/shaders/src/commonMain/kotlin/ShaderSources.kt
@@ -4,10 +4,10 @@ internal object ShaderSources {
     layout(color) uniform half4 color;
     layout(color) uniform half4 color2;
 
-    half4 main(in float2 fragCoord) {
+    half4 main(float2 fragCoord) {
         float2 uv = fragCoord/resolution.xy;
 
-        float mixValue = distance(uv, vec2(0, 1));
+        float mixValue = distance(uv, float2(0, 1));
         return mix(color, color2, mixValue);
     }
 """.trimIndent()
