3ed678bf050821822290e57779d7805ac193bf1d
stashymane <me@stashy.dev>
2026-08-06 17:52:23 +0300

    remove mask from commit graph

--- composeApp/src/commonMain/kotlin/screens/about/components/BusinessCard.kt
+++ composeApp/src/commonMain/kotlin/screens/about/components/BusinessCard.kt
@@ -16,11 +16,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithCache
-import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.CompositingStrategy.Companion.Offscreen
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.font.FontWeight.Companion.Black
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.unit.dp
@@ -35,8 +32,6 @@ import ui.preview.PreviewData
 import ui.preview.PreviewHost
 import ui.theme.easeGradientBetween
 import ui.theme.fullRadialGradient
-
-private val maskGradient = easeGradientBetween(Color.White, Color.Transparent)
 
 @Composable
 fun BusinessCard(
@@ -54,22 +49,7 @@ fun BusinessCard(
         ContributionGraph(
             graph = meta.contributionGraph,
             peakColor = MaterialTheme.colorScheme.surfaceContainerHigh,
-            modifier = Modifier.matchParentSize().padding(4.dp)
-                .graphicsLayer(compositingStrategy = Offscreen)
-                .drawWithCache {
-                    val mask = Brush.fullRadialGradient(
-                        *maskGradient,
-                        size = size,
-                        radius = 1f,
-                        x = 1f,
-                        y = 1f
-                    )
-
-                    onDrawWithContent {
-                        drawContent()
-                        drawRect(mask, blendMode = BlendMode.DstIn)
-                    }
-                },
+            modifier = Modifier.matchParentSize().padding(4.dp),
             gapFraction = 0.05f
         )
 
